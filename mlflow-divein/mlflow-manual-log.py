f18d649d405cb11573a6cd369863407075128253
Shaz <shatadrusaha@gmail.com>
2025-06-13 21:31:15 +0100

    mlflow manual-logging wip.

--- mlflow-divein/mlflow-manual-log.py
+++ mlflow-divein/mlflow-manual-log.py
@@ -1,42 +1,26 @@
-### Auto-logging
-
-
-"""                     Import libraries.                       """
-from datetime import datetime as dt
-import numpy as np
-import mlflow
 """
 LightGBM library requires OpenMP library (`libomp.dylib`) on Mac. To get this, install `libomp` using `Homebrew`.
 Open the terminal and run: 
     `brew install libomp`
 """
-import lightgbm as lgbm
-from sklearn.datasets import load_breast_cancer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+
+
+"""                     Import libraries.                       """
+from datetime import datetime as dt  # noqa: E402
+import mlflow  # noqa: E402
+from mlflow.models import infer_signature  # noqa: E402
+import lightgbm as lgbm  # noqa: E402
+from sklearn.datasets import load_breast_cancer  # noqa: E402
+from sklearn.model_selection import train_test_split  # noqa: E402
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score  # noqa: E402
 
 
 """                     User defined variables.                       """
 # Random seed.
 random_seed = 14
 
-"""
-https://mlflow.org/docs/latest/ml/tracking/quickstart
-https://youtu.be/cjeCAoW83_U?si=xZ663D9-AuIU5QyD
-
-Run a local Tracking Server -
-    Start a local MLflow Tracking Server, which will be connected to for logging. From a terminal, run:
-        `mlflow server --host 127.0.0.1 --port 8080`
-        `mlflow server --host localhost --port 8080`
-
-Set the Tracking Server URI -
-    Setup the MLflow Tracking Server URI to point to the local server. This is done in the code below.
-        `mlflow.set_tracking_uri(uri="http://<host>:<port>")`
-        `mlflow.set_tracking_uri(uri="http://localhost:8080")`
-
-"""
-
 # Mlflow.
+# TODO - Make sure to start the mlflow server/ui on the specific port first.
 mlflow_tracking_uri = 'http://localhost:8080'
 mlflow_exp_name = 'mlflow-get-started'
 # Set the MLflow Tracking Server URI.
@@ -67,6 +51,10 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # Ignoring any transformations (like, scaling) on the dataset for simplicity.
 
+# Create a LightGBM dataset.
+train_dataset = lgbm.Dataset(X_train, label=y_train)
+test_dataset = lgbm.Dataset(X_test, label=y_test, reference=train_dataset)
+
 # Define parameters for the LightGBM model.
 # https://lightgbm.readthedocs.io/en/latest/Parameters.html#parameters-format
 params = {
@@ -77,38 +65,21 @@ params = {
     'learning_rate': 0.05, # Learning rate for the model.
     'num_leaves': 31, # Maximum number of leaves in one tree.
     'max_depth': -1, # Maximum depth of the tree. -1 means no limit.
-    'feature_fraction': 0.9, # Fraction of features to be used for each boosting iteration.
+    'feature_fraction': 0.8, # Fraction of features to be used for each boosting iteration.
     'early_stopping_round': 20, # Early stopping after 20 rounds if no improvement in validation loss.
     # 'bagging_fraction': 0.8, # Fraction of data to be used for each boosting iteration.
     # 'bagging_freq': 5, # Frequency of bagging. 0 means no bagging.
     # 'lambda_l1': 0.0, # L1 regularization term on weights.
     # 'lambda_l2': 0.0, # L2 regularization term on weights.
-    'metric': 'binary_logloss', # auc, average_precision, binary_logloss
+    'metric': 'auc', # auc, average_precision, binary_logloss
 }
-
-# Create a LightGBM dataset.
-train_dataset = lgbm.Dataset(X_train, label=y_train)
-test_dataset = lgbm.Dataset(X_test, label=y_test, reference=train_dataset)
-
-# Define the MLflow run name.
-mlflow_run_name = f"mlf-autolog-{dt.now().strftime('%Y%m%d-%H%M%S')}"
-
-
-# """                     Start an MLflow run - 'auto logging'.                       """
-# # Start an MLflow run with the defined experiment name and run name.
-# mlflow.set_experiment(mlflow_exp_name)
-# with mlflow.start_run(run_name=mlflow_run_name) as run:
-#     # Log parameters to MLflow.
-#     mlflow.log_params(params)
-    
-#     # Enable auto-logging for LightGBM.
-#     mlflow.lightgbm.autolog()
-# # Train the LightGBM model.
 
 
 """                     Start an MLflow run - 'manual logging'.                       """
 # Start an MLflow run with the defined experiment name and run name.
 mlflow.set_experiment(mlflow_exp_name)
+mlflow_run_name = f"mlf-manual-log-{dt.now().strftime('%Y%m%d-%H%M%S')}"
+
 with mlflow.start_run(run_name=mlflow_run_name) as run:
     # Log parameters to MLflow.
     mlflow.log_params(params)
@@ -120,11 +91,19 @@ with mlflow.start_run(run_name=mlflow_run_name) as run:
         valid_sets=[test_dataset],
         # verbose_eval=False
     )
-    mlflow.lightgbm.log_model(lgb_model=model_lgbm, artifact_path="model")
+    signature = infer_signature(
+        model_input=X_train,
+        model_output=model_lgbm.predict(data=X_train)
+    )
+    mlflow.lightgbm.log_model(
+        lgb_model=model_lgbm, 
+        artifact_path="model",
+        signature=signature,
+    )
 
     # Predict on train and test datasets.
-    y_train_pred = model_lgbm.predict(X_train)
-    y_test_pred = model_lgbm.predict(X_test)
+    y_train_pred = model_lgbm.predict(data=X_train)
+    y_test_pred = model_lgbm.predict(data=X_test)
 
     y_train_pred_binary = (y_train_pred >= 0.5).astype(int)
     y_test_pred_binary = (y_test_pred >= 0.5).astype(int)
